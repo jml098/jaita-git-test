a6b8d3d4b381d9f4ae8a69dadd199aa2e69656a8
Giacomotestori <giacomo.testori10@gmail.com>
2023-10-03 15:50:41 +0200

    PUSH TEST

---
+++ src/main/java/jaitagittest/jaitagittest/JaitaGitTestApplication.java
@@ -0,0 +1,13 @@
+package jaitagittest.jaitagittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaitaGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaitaGitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaitagittest/jaitagittest/JaitaGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jaitagittest.jaitagittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaitaGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
